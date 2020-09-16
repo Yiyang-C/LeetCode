0882f80d606ffb1dff756ff868fb655fdbec5e4c
Yiyang-C <60457857+Yiyang-C@users.noreply.github.com>
2020-09-15 18:41:40 -0700

    Update and rename 0058.py to 0058.md

--- 0058.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        s = s.strip()
-        # print(s)
-        if not s:
-            return 0
-        word = s.split()
-        return len(word[-1])
